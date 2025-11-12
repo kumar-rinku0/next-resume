40c46260a4ec45ccfed92530886cf731947be025
kumar-rinku0 <rinkukumar412616@gmail.com>
2025-11-13 00:46:41 +0530

    nextjs resume created

---
+++ app/layout.tsx
@@ -0,0 +1,49 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+import { generalData } from "@/data/general";
+import type { Metadata } from "next";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: `${generalData.name} - ${generalData.jobTitle}`,
+  description: generalData.about,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "",
+    siteName: `${generalData.name} - ${generalData.jobTitle}`,
+    title: `${generalData.name} - ${generalData.jobTitle}`,
+    description: generalData.about,
+    images: [
+      {
+        url: "",
+        width: 1200,
+        height: 630,
+        alt: `${generalData.name} - ${generalData.jobTitle}`,
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} dark:bg-neutral-900`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
